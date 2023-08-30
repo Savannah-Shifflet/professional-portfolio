d274d4afbf8ce29b7f3fdba674428276e570afa9
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-08-30 11:48:22 -0500

    established react router to direct page routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 // import components 
 import React from "react";
-import Home from "./components/pages/Home";
+import Menu from './components/Menu';
 import Footer from "./components/Footer";
-import Menu from "./components/Menu"
+import {Routes, Route } from "react-router-dom";
+import Home from './components/pages/Home';
+import Portfolio from './components/pages/Portfolio';
+import About from './components/pages/About';
+import Resume from './components/pages/Resume';
+
 
 function App() {
     return(
         <div >
-            < Menu />
-            < Home /> 
-            < Footer />
+            <Menu/>
+            <Routes>
+                <Route path = '/' >
+                    <Route index element={<Home />} />
+                    <Route path='about-me' element={<About />} />
+                    <Route path='portfolio' element={<Portfolio />} />
+                    <Route path='resume' element={<Resume />} />
+                </Route>
+            </Routes>
+            <Footer />
         </div>
     )
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
 import Link from '@mui/material/Link';
+
 
 export default function Menu() {
     // TODO: resize menu for smaller screens
@@ -21,19 +20,15 @@ export default function Menu() {
 
     // return <div>`the width is ${dimensions.width} and the height is ${dimensions.height}`</div>;
 
-    const [value, setValue] = useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
     return (
+        <div>
             <Box height = '100vh' alignItems = 'center' sx = {{position: 'fixed', right: 12, display:'flex'}} >
                 <Box sx = {{writingMode: 'vertical-rl', textOrientation: 'revert', transform: 'rotate(180deg)'}}>
-                    <Link href="/about-me">About Me</Link>
-                    <Link href="/portfolio">Portfolio</Link>
-                    <Link href="/resume">Resume</Link>
+                    <Link href="/about-me" >About Me</Link>
+                    <Link href="/portfolio" >Portfolio</Link>
+                    <Link href="/resume" >Resume</Link>
                 </Box>
             </Box>
-    )
+        </div>
+)
 }
