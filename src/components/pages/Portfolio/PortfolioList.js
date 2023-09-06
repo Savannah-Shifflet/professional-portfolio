d6fd0648718f5a696902125ca38b2dd56ba9c580
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-09-06 12:01:18 -0500

    updated formatting for mobile layout and included summary for portfolio page

--- src/components/pages/Portfolio/PortfolioList.js
+++ src/components/pages/Portfolio/PortfolioList.js
@@ -17,7 +17,6 @@ export default function PortfolioList({ pieces }) {
             margin: 3
         }
     }
-    console.log(pieces)
     return (
         <Grid item container xs ={12} justifyContent={'center'} >
             {/* map each portfolio piece to formatting section */}
@@ -26,32 +25,42 @@ export default function PortfolioList({ pieces }) {
                 if(index === pieces.length-1){
                     isLast = true;
                 }
-                console.log(index);
-                console.log(isLast)
                 return(
                 <Grid item container md={9} xs ={11} key={piece.title} sx={{m: 3}}>
-                    {/* gif and title take up one row */}
-                    <Grid item xs={7}>
+                    {/* gif, title, summary take up one row */}
+                    <Grid item xs={11} md={7}>
                         <img src={piece.media} alt ='gif of portfolio piece' style ={styles.gif}/>
                     </Grid>
-                    <Grid item xs = {5} sx ={{textAlign: 'right'}} >
-                        <Typography variant ='h3'>{piece.title}</Typography>
+                    <Grid container item xs = {11} md={5}  justifyContent='flex-end'>
+                        <Grid item xs={11} sx={{height: 'fit-content'}}>
+                            <Typography variant ='h3' sx={{textAlign: 'right'}}>{piece.title}</Typography>
+                        </Grid>
+                        <Grid item xs={11}>
+                            <Typography variant ='body1'>{piece.summary}</Typography>
+                        </Grid>
+                        
                     </Grid>
+
+                    {/* list of technology badges used for the project */}
+                    <Grid container item xs ={12} sx={{mt:'2.5%'}}>
+                            {piece.technology.map((tech)=>{
+                                return(
+                                    <Grid item sx={{height:'fit-content'}}>
+                                        <img key= {tech.alt} src={tech.source} alt = {tech.alt} style ={styles.badge}/>
+                                    </Grid>)
+                            })}
+                    </Grid>
+
                     {/* Links to the piece's github and deployed project are second row */}
-                    <Grid item container xs ={12} sx={{my:2}} >
-                        <Grid container item xs ={6} justifyContent='center'>
+                    <Grid item container xs ={11} md={12}sx={{my:2}} justifyContent='space-between'>
+                        <Grid container item xs ={5} md={6} justifyContent='center'>
                             <Button target = "_blank" href={piece.github} variant="project">GitHub Repository</Button>
                         </Grid>
-                        <Grid container item xs ={6} justifyContent='center'>
+                        <Grid container item xs ={5} md={6} justifyContent='center'>
                             <Button variant='project' target = "_blank" href={piece.deployed} >Deployed Project</Button>
                         </Grid>
                     </Grid>
-                    {/* list of technology badges used for the project */}
-                    <Grid item xs ={12} >
-                        {piece.technology.map((tech)=>{
-                            return(<img key= {tech.alt} src={tech.source} alt = {tech.alt} style ={styles.badge}/>)
-                        })}
-                    </Grid>
+                    
                     <Grid item xs ={12} sx={{mt:'2.5%'}}>
                         { !isLast ? (<Divider  sx={{bgcolor: 'accent.main', height: '0.2em'}}/>) : (<div></div>)}
                     </Grid>
