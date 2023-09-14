9fd49f3920cf43db15655a87a127ebd1e632505b
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-09-14 13:05:35 -0500

    updated with new project

--- src/components/pages/Portfolio/data/CurrentPieces.js
+++ src/components/pages/Portfolio/data/CurrentPieces.js
@@ -1,44 +1,7 @@
 import portfolio from '../../../../assets/portfolio.gif';
-import sidekick from '../../../../assets/sidekick.gif';
+
 
 export const currentPieces = [
-    {
-        title: 'Sidekick',
-        media: sidekick,
-        github: 'https://github.com/AlinaB108/pos-system-5000',
-        deployed: '',
-        summary: 'A restaurant POS application that utilizes a full MERN stack to drive efficient ordering and sales.',
-        disabled: true,
-        technology: [
-            { source: 'https://img.shields.io/badge/JavaScript-F7DF1E.svg?style=for-the-badge&logo=JavaScript&logoColor=black',
-            alt: 'JavaScript'
-            },
-            { source: 'https://img.shields.io/badge/Node.js-339933.svg?style=for-the-badge&logo=nodedotjs&logoColor=white',
-            alt: 'Node.Js'
-            },
-            { source: 'https://img.shields.io/badge/React-61DAFB.svg?style=for-the-badge&logo=React&logoColor=black',
-            alt: 'React'
-            },
-            { source: 'https://img.shields.io/badge/MongoDB-47A248.svg?style=for-the-badge&logo=MongoDB&logoColor=white',
-            alt: 'MongoDB'
-            },
-            { source: 'https://img.shields.io/badge/Mongoose-880000.svg?style=for-the-badge&logo=Mongoose&logoColor=white',
-            alt: 'Mongoose'
-            },
-            { source: 'https://img.shields.io/badge/Apollo%20GraphQL-311C87.svg?style=for-the-badge&logo=Apollo-GraphQL&logoColor=white',
-            alt: 'GraphQL'
-            },
-            { source: 'https://img.shields.io/badge/JSON%20Web%20Tokens-000000.svg?style=for-the-badge&logo=JSON-Web-Tokens&logoColor=white',
-            alt: 'JSON Web Tokens'
-            },
-            { source: 'https://img.shields.io/badge/React%20Router-CA4245.svg?style=for-the-badge&logo=React-Router&logoColor=white',
-            alt: 'React Router'
-            },
-            { source: 'https://img.shields.io/badge/Material--UI-0081CB?style=for-the-badge&logo=mui&logoColor=white',
-            alt: 'Material UI'
-            }
-        ],
-    },
     {
         title: 'Portfolio Website',
         media: portfolio,
