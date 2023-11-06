096d4d66f1c47703b14a215af1071c0dfe76af85
Savannah-Shifflet <savannah.shifflet@gmail.com>
2023-11-06 10:15:13 -0600

    added honors to resume page

--- src/components/pages/Resume/Honors.js
+++ src/components/pages/Resume/Honors.js
@@ -2,51 +2,38 @@ import React from 'react';
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
 
 
 export default function Honors() {
 
-    const education = [
-        {
-            title: 'Professional Certificate: Full-stack coding',
-            school: "The University of Texas at Austin",
-            time: "June 2023 – September 2023"
-        },
-        {
-            title: 'Pre-Medical Post-Baccalaureate',
-            school: "Austin Community College & The University of Texas at Austin ",
-            gpa: "4.00",
-            mcat: '94th percentile',
-            time: "September 2019 – December 2021"
-        },
-        {
-            title: 'Bachelors of Science in Social Work',
-            school: "The University of Texas at Austin",
-            gpa: '3.99',
-            time: "August 2015 – May 2019"
-        }
+    const honors = [
+        'Graduated UT with Highest Honors',
+        'Valedictorian – Dripping Springs High School',
+        "2021 ACC Chancellor’s Honor Award",
+        "2018-2019 UT HEALS Scholar",
+        "2018-2019 Tu-Ting and Rachel Tsan Endowed Presidential Scholar",
+        "2017-2018, 2018-2019 UT Distinguished College Scholar",
+        "2017-2018 School of Social Work Anniversary Excellence Fund recipient",
+        "2016-2017 UT College Scholar"
     ];
 
     return (
         <Grid container item xs={12} sx={{ mt: '0.5%' }} >
-            {education.map((education) => {
+            <List >
+            {honors.map((honor) => {
                 return(
-                    <>
-                    <Grid item container justifyContent={'space-between'} key={education.title} xs={12} lg={6} sx ={{pt: '0.2em', pb: '1em', pr:'1.4em'}}>
-                        <Grid item xs={12}>
-                            <Typography variant='body1' >{education.title} {education.gpa? (`| GPA: ${education.gpa}`):null}{education.mcat? (`| MCAT: ${education.mcat}`):null}</Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography variant='body1'>{education.time}</Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography variant='body1'>{education.school}</Typography>
-                        </Grid>
-                    </Grid>
-
-                    </>
+                    <ListItem sx={{pt: 0, pb: 0}}>
+                        <ListItemText
+                            primary={honor}
+                        />
+                    </ListItem>
                 )
             })}
+            </List>
         </Grid>
     )
 }
